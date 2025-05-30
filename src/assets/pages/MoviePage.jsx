c8a29190ba73a40501addddf3926cc7a66f00c2f
Ivanaricci <ivana.ricci26@gmail.com>
2025-05-30 17:38:30 +0200

    [UPD] Add image in detail movie

--- src/assets/pages/MoviePage.jsx
+++ src/assets/pages/MoviePage.jsx
@@ -4,7 +4,7 @@ const MoviePage = () => {
     return (
         <div className='row'>
             <div className="col-12 col-md-6 col-lg-4">
-                Immagine
+                <img src="https://picsum.photos/500/300" alt="Movie" className='img-fluid' />
             </div>
             <div className="col-12 col-md-6 col-lg-8">
                 <h1>Titolo</h1>
