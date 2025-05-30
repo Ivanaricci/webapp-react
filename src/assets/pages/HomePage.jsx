df0757f6df34d4daa07aa91d155ce0c5e3de5adf
Ivanaricci <ivana.ricci26@gmail.com>
2025-05-30 17:34:12 +0200

    [UPD] Add card in homepage

--- src/assets/pages/HomePage.jsx
+++ src/assets/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const HomePage = () => {
     return (
@@ -7,7 +8,103 @@ const HomePage = () => {
             <h2>
                 <i>List</i>
             </h2>
-            <div className="row">{/*inseriamo i film*/}</div>
+            <div className="row gy-4">
+                <div className="col-12 col-md-6 col-lg-4">
+                    <div className="card">
+                        <div className="card-image-top">
+                            <img src="https://picsum.photos/500/300" alt="Movie" className='img-fluid' />
+                        </div>
+                        <div className="card-body">
+                            <h3 className='text-primary'>Titolo</h3>
+                            <h4>
+                                <em>Genere</em>
+                            </h4>
+                            <p>Descrizione</p>
+                            <Link className="btn btn-primary" to="/movies/1">Leggi tutto</Link>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-12 col-md-6 col-lg-4">
+                    <div className="card">
+                        <div className="card-image-top">
+                            <img src="https://picsum.photos/500/300" alt="Movie" className='img-fluid' />
+                        </div>
+                        <div className="card-body">
+                            <h3 className='text-primary'>Titolo</h3>
+                            <h4>
+                                <em>Genere</em>
+                            </h4>
+                            <p>Descrizione</p>
+                            <Link className="btn btn-primary" to="/movies/1">Leggi tutto</Link>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-12 col-md-6 col-lg-4">
+                    <div className="card">
+                        <div className="card-image-top">
+                            <img src="https://picsum.photos/500/300" alt="Movie" className='img-fluid' />
+                        </div>
+                        <div className="card-body">
+                            <h3 className='text-primary'>Titolo</h3>
+                            <h4>
+                                <em>Genere</em>
+                            </h4>
+                            <p>Descrizione</p>
+                            <Link className="btn btn-primary" to="/movies/1">Leggi tutto</Link>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-12 col-md-6 col-lg-4">
+                    <div className="card">
+                        <div className="card-image-top">
+                            <img src="https://picsum.photos/500/300" alt="Movie" className='img-fluid' />
+                        </div>
+                        <div className="card-body">
+                            <h3 className='text-primary'>Titolo</h3>
+                            <h4>
+                                <em>Genere</em>
+                            </h4>
+                            <p>Descrizione</p>
+                            <Link className="btn btn-primary" to="/movies/1">Leggi tutto</Link>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-12 col-md-6 col-lg-4">
+                    <div className="card">
+                        <div className="card-image-top">
+                            <img src="https://picsum.photos/500/300" alt="Movie" className='img-fluid' />
+                        </div>
+                        <div className="card-body">
+                            <h3 className='text-primary'>Titolo</h3>
+                            <h4>
+                                <em>Genere</em>
+                            </h4>
+                            <p>Descrizione</p>
+                            <Link className="btn btn-primary" to="/movies/1">Leggi tutto</Link>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-12 col-md-6 col-lg-4">
+                    <div className="card">
+                        <div className="card-image-top">
+                            <img src="https://picsum.photos/500/300" alt="Movie" className='img-fluid' />
+                        </div>
+                        <div className="card-body">
+                            <h3 className='text-primary'>Titolo</h3>
+                            <h4>
+                                <em>Genere</em>
+                            </h4>
+                            <p>Descrizione</p>
+                            <Link className="btn btn-primary" to="/movies/1">Leggi tutto</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
