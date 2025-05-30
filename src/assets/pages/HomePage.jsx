bb2445b44bf2fdfa7cee32ffffd76c73f5030d83
Ivanaricci <ivana.ricci26@gmail.com>
2025-05-30 19:00:06 +0200

    [UPD] Add array ciclato per mostrare i film

--- src/assets/pages/HomePage.jsx
+++ src/assets/pages/HomePage.jsx
@@ -1,7 +1,70 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+
+
+const initialMovies = [
+    {
+        id: 1,
+        title: "Titolo1",
+        director: "Direttore1",
+        genre: "Action",
+        release_year: "2003",
+        abstract: "Lorem ipsum",
+        image: "https://picsum.photos/500/300"
+    },
+    {
+        id: 2,
+        title: "Titolo2",
+        director: "Direttore2",
+        genre: "Action",
+        release_year: "2003",
+        abstract: "Lorem ipsum",
+        image: "https://picsum.photos/500/300"
+    },
+    {
+        id: 3,
+        title: "Titolo3",
+        director: "Direttore3",
+        genre: "Action",
+        release_year: "2003",
+        abstract: "Lorem ipsum",
+        image: "https://picsum.photos/500/300"
+    },
+    {
+        id: 4,
+        title: "Titolo4",
+        director: "Direttore4",
+        genre: "Action",
+        release_year: "2003",
+        abstract: "Lorem ipsum",
+        image: "https://picsum.photos/500/300"
+    },
+    {
+        id: 5,
+        title: "Titolo5",
+        director: "Direttore5",
+        genre: "Action",
+        release_year: "2003",
+        abstract: "Lorem ipsum",
+        image: "https://picsum.photos/500/300"
+    },
+    {
+        id: 6,
+        title: "Titolo6",
+        director: "Direttore6",
+        genre: "Action",
+        release_year: "2003",
+        abstract: "Lorem ipsum",
+        image: "https://picsum.photos/500/300"
+    },
+]
 
 const HomePage = () => {
+
+    // Definisco la variabile di stato
+    const [movies, setMovies] = useState(initialMovies);
+
     return (
         <>
             <h1 className='text-primary'>Movie</h1>
@@ -9,101 +72,32 @@ const HomePage = () => {
                 <i>List</i>
             </h2>
             <div className="row gy-4">
-                <div className="col-12 col-md-6 col-lg-4">
-                    <div className="card">
-                        <div className="card-image-top">
-                            <img src="https://picsum.photos/500/300" alt="Movie" className='img-fluid' />
+                {movies.map((movie) => {
+                    return (
+                        <div className="col-12 col-md-6 col-lg-4">
+                            <div className="card">
+                                <div className="card-image-top">
+                                    <img src={movie.image} alt="Movie" className='img-fluid' />
+                                </div>
+                                <div className="card-body">
+                                    <h3 className='text-primary'>{movie.title}</h3>
+                                    <h5>
+                                        <em className='text-secondary'>{movie.director}</em>
+                                    </h5>
+                                    <h4>
+                                        <em>{movie.genre}</em>
+                                    </h4>
+                                    <h6>
+                                        <em className='text-secondary'>{movie.release_year}</em>
+                                    </h6>
+                                    <p>{movie.abstract}</p>
+                                    <Link className="btn btn-primary" to="/movies/1">Leggi tutto</Link>
+                                </div>
+                            </div>
                         </div>
-                        <div className="card-body">
-                            <h3 className='text-primary'>Titolo</h3>
-                            <h4>
-                                <em>Genere</em>
-                            </h4>
-                            <p>Descrizione</p>
-                            <Link className="btn btn-primary" to="/movies/1">Leggi tutto</Link>
-                        </div>
-                    </div>
-                </div>
+                    )
+                })}
 
-                <div className="col-12 col-md-6 col-lg-4">
-                    <div className="card">
-                        <div className="card-image-top">
-                            <img src="https://picsum.photos/500/300" alt="Movie" className='img-fluid' />
-                        </div>
-                        <div className="card-body">
-                            <h3 className='text-primary'>Titolo</h3>
-                            <h4>
-                                <em>Genere</em>
-                            </h4>
-                            <p>Descrizione</p>
-                            <Link className="btn btn-primary" to="/movies/1">Leggi tutto</Link>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="col-12 col-md-6 col-lg-4">
-                    <div className="card">
-                        <div className="card-image-top">
-                            <img src="https://picsum.photos/500/300" alt="Movie" className='img-fluid' />
-                        </div>
-                        <div className="card-body">
-                            <h3 className='text-primary'>Titolo</h3>
-                            <h4>
-                                <em>Genere</em>
-                            </h4>
-                            <p>Descrizione</p>
-                            <Link className="btn btn-primary" to="/movies/1">Leggi tutto</Link>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="col-12 col-md-6 col-lg-4">
-                    <div className="card">
-                        <div className="card-image-top">
-                            <img src="https://picsum.photos/500/300" alt="Movie" className='img-fluid' />
-                        </div>
-                        <div className="card-body">
-                            <h3 className='text-primary'>Titolo</h3>
-                            <h4>
-                                <em>Genere</em>
-                            </h4>
-                            <p>Descrizione</p>
-                            <Link className="btn btn-primary" to="/movies/1">Leggi tutto</Link>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="col-12 col-md-6 col-lg-4">
-                    <div className="card">
-                        <div className="card-image-top">
-                            <img src="https://picsum.photos/500/300" alt="Movie" className='img-fluid' />
-                        </div>
-                        <div className="card-body">
-                            <h3 className='text-primary'>Titolo</h3>
-                            <h4>
-                                <em>Genere</em>
-                            </h4>
-                            <p>Descrizione</p>
-                            <Link className="btn btn-primary" to="/movies/1">Leggi tutto</Link>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="col-12 col-md-6 col-lg-4">
-                    <div className="card">
-                        <div className="card-image-top">
-                            <img src="https://picsum.photos/500/300" alt="Movie" className='img-fluid' />
-                        </div>
-                        <div className="card-body">
-                            <h3 className='text-primary'>Titolo</h3>
-                            <h4>
-                                <em>Genere</em>
-                            </h4>
-                            <p>Descrizione</p>
-                            <Link className="btn btn-primary" to="/movies/1">Leggi tutto</Link>
-                        </div>
-                    </div>
-                </div>
             </div>
         </>
     )
