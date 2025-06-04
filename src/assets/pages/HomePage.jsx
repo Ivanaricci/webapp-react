ebc947617ebff30c7087c7d5bf43e7ee7da3c6a9
Ivanaricci <ivana.ricci26@gmail.com>
2025-06-04 17:36:02 +0200

    [UPD] Movies in page

--- src/assets/pages/HomePage.jsx
+++ src/assets/pages/HomePage.jsx
@@ -4,70 +4,16 @@ import MovieCard from '../../components/MovieCard'
 import axios from 'axios'
 
 
-const initialMovies = [
-    {
-        id: 1,
-        title: "Titolo1",
-        director: "Direttore1",
-        genre: "Action",
-        release_year: "2003",
-        abstract: "Lorem ipsum",
-        image: "https://picsum.photos/500/300"
-    },
-    {
-        id: 2,
-        title: "Titolo2",
-        director: "Direttore2",
-        genre: "Action",
-        release_year: "2003",
-        abstract: "Lorem ipsum",
-        image: "https://picsum.photos/500/300"
-    },
-    {
-        id: 3,
-        title: "Titolo3",
-        director: "Direttore3",
-        genre: "Action",
-        release_year: "2003",
-        abstract: "Lorem ipsum",
-        image: "https://picsum.photos/500/300"
-    },
-    {
-        id: 4,
-        title: "Titolo4",
-        director: "Direttore4",
-        genre: "Action",
-        release_year: "2003",
-        abstract: "Lorem ipsum",
-        image: "https://picsum.photos/500/300"
-    },
-    {
-        id: 5,
-        title: "Titolo5",
-        director: "Direttore5",
-        genre: "Action",
-        release_year: "2003",
-        abstract: "Lorem ipsum",
-        image: "https://picsum.photos/500/300"
-    },
-    {
-        id: 6,
-        title: "Titolo6",
-        director: "Direttore6",
-        genre: "Action",
-        release_year: "2003",
-        abstract: "Lorem ipsum",
-        image: "https://picsum.photos/500/300"
-    },
-]
+
 
 const HomePage = () => {
 
     // Definisco la variabile di stato
-    const [movies, setMovies] = useState(initialMovies);
+    const [movies, setMovies] = useState([]);
 
     useEffect(() => {
         axios.get('http://localhost:3000/api/movies').then((resp) => {
+            setMovies(resp.data)
             console.log(resp.data)
         }).catch((err) => {
             console.log(err)
