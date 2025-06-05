08c7d952843279922e9fac9fdf67c5b2f147aa70
Ivanaricci <ivana.ricci26@gmail.com>
2025-06-05 18:51:38 +0200

    [UPD] Add chiamata per la pagina di dettaglio

--- src/assets/pages/MoviePage.jsx
+++ src/assets/pages/MoviePage.jsx
@@ -2,222 +2,25 @@ import React, { useEffect } from 'react'
 import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import ReviewCard from '../../components/ReviewCard'
-
-const initialMovies = [
-    {
-        id: 1,
-        title: "Titolo1",
-        director: "Direttore1",
-        genre: "Action",
-        release_year: "2003",
-        abstract: "Lorem ipsum",
-        image: "https://picsum.photos/500/300",
-        reviews: [
-            {
-                id: 1,
-                text: "Lorem ipsum",
-                author: "Nome1",
-                vote: 1
-            },
-            {
-                id: 2,
-                text: "Lorem ipsum",
-                author: "Nome2",
-                vote: 1
-            },
-            {
-                id: 3,
-                text: "Lorem ipsum",
-                author: "Nome3",
-                vote: 1
-            },
-        ]
-    },
-    {
-        id: 2,
-        title: "Titolo2",
-        director: "Direttore2",
-        genre: "Action",
-        release_year: "2003",
-        abstract: "Lorem ipsum",
-        image: "https://picsum.photos/500/300",
-        reviews: [
-            {
-                id: 4,
-                text: "Lorem ipsum",
-                author: "Nome4",
-                vote: 2
-            },
-            {
-                id: 5,
-                text: "Lorem ipsum",
-                author: "Nome5",
-                vote: 2
-            },
-            {
-                id: 6,
-                text: "Lorem ipsum",
-                author: "Nome6",
-                vote: 2
-            },
-        ]
-    },
-    {
-        id: 3,
-        title: "Titolo3",
-        director: "Direttore3",
-        genre: "Action",
-        release_year: "2003",
-        abstract: "Lorem ipsum",
-        image: "https://picsum.photos/500/300",
-        reviews: [
-            {
-                id: 7,
-                text: "Lorem ipsum",
-                author: "Nome7",
-                vote: 3
-            },
-            {
-                id: 8,
-                text: "Lorem ipsum",
-                author: "Nome8",
-                vote: 3
-            },
-            {
-                id: 9,
-                text: "Lorem ipsum",
-                author: "Nome9",
-                vote: 3
-            },
-        ]
-    },
-    {
-        id: 4,
-        title: "Titolo4",
-        director: "Direttore4",
-        genre: "Action",
-        release_year: "2003",
-        abstract: "Lorem ipsum",
-        image: "https://picsum.photos/500/300",
-        reviews: [
-            {
-                id: 10,
-                text: "Lorem ipsum",
-                author: "Nome10",
-                vote: 4
-            },
-            {
-                id: 11,
-                text: "Lorem ipsum",
-                author: "Nome11",
-                vote: 4
-            },
-            {
-                id: 12,
-                text: "Lorem ipsum",
-                author: "Nome12",
-                vote: 4
-            },
-        ]
-    },
-    {
-        id: 5,
-        title: "Titolo5",
-        director: "Direttore5",
-        genre: "Action",
-        release_year: "2003",
-        abstract: "Lorem ipsum",
-        image: "https://picsum.photos/500/300",
-        reviews: [
-            {
-                id: 13,
-                text: "Lorem ipsum",
-                author: "Nome13",
-                vote: 5
-            },
-            {
-                id: 14,
-                text: "Lorem ipsum",
-                author: "Nome14",
-                vote: 5
-            },
-            {
-                id: 15,
-                text: "Lorem ipsum",
-                author: "Nome15",
-                vote: 5
-            },
-        ]
-    },
-    {
-        id: 6,
-        title: "Titolo6",
-        director: "Direttore6",
-        genre: "Action",
-        release_year: "2003",
-        abstract: "Lorem ipsum",
-        image: "https://picsum.photos/500/300",
-        reviews: [
-            {
-                id: 16,
-                text: "Lorem ipsum",
-                author: "Nome16",
-                vote: 6
-            },
-            {
-                id: 17,
-                text: "Lorem ipsum",
-                author: "Nome17",
-                vote: 6
-            },
-            {
-                id: 18,
-                text: "Lorem ipsum",
-                author: "Nome18",
-                vote: 6
-            },
-        ]
-    },
-]
+import axios from 'axios'
 
 
-const initialReviews = [
-    {
-        id: 1,
-        text: "Lorem ipsum",
-        author: "Nome1",
-        vote: 3
-    },
-    {
-        id: 2,
-        text: "Lorem ipsum",
-        author: "Nome2",
-        vote: 5
-    },
-    {
-        id: 3,
-        text: "Lorem ipsum",
-        author: "Nome3",
-        vote: 2
-    },
-]
 
 const MoviePage = () => {
 
     const { id } = useParams();
-    const [movies, setMovies] = useState(initialMovies);
+
 
 
     const [movie, setMovie] = useState(null);
 
     // funzione che recupera l'array che ha l'id passato come parametro
     const fetchMovie = () => {
-        movies.forEach((actualMovie) => {
-            if (actualMovie.id === parseInt(id)) {
-                setMovie(actualMovie);
-            }
+        axios.get(`http://127.0.0.1:3000/api/movies/${id}`).then((resp) => {
+            setMovie(resp.data);
         })
-    }
+            .catch((err) => console.log(err));
+    };
 
     useEffect(() => {
         fetchMovie()
@@ -254,8 +57,8 @@ const MoviePage = () => {
                             </div>
                         </div>
                         {movie.reviews.map((review) => (
-                            <div className="row gy-3">
-                                <ReviewCard review={review} key={`review -${review.id}`} />
+                            <div className="row gy-3" key={`review -${review.id}`}>
+                                <ReviewCard review={review} />
                             </div>
                         ))}
                     </>
--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -7,7 +7,7 @@ const ReviewCard = ({ review }) => {
                 <div className="card p-4">
                     <p>{review.text}</p>
                     <p>{review.vote}</p>
-                    <p>{review.author}</p>
+                    <p>{review.name}</p>
                 </div>
             </div>
         </div>
